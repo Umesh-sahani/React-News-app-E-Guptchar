050a763b5981dd987a7faf52d6bd377e9dd1c076
Umesh Sahani <umeshsahani1998@gmail.com>
2021-11-12 23:08:48 +0530

    added react-router-dom and updated app.js and mmany more changes

--- src/components/News.js
+++ src/components/News.js
@@ -2,80 +2,25 @@
 import React, { Component } from 'react'
 import NewsBox from './NewsBox'
 import Spinner from './Spinner'
+import PropTypes  from 'prop-types';
 
 export default class News extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "the-times-of-india",
-                "name": "The Times of India"
-            },
-            "author": "TIMESOFINDIA.COM",
-            "title": "Centre cuts excise duty on petrol, diesel - Times of India",
-            "description": "India Business News: In a big relief to consumers ahead of Diwali, the government on Wednesday announced reduction in excise duty on petrol and diesel.Excise duty on petro",
-            "url": "https://timesofindia.indiatimes.com/business/india-business/centre-cuts-excise-duty-on-petrol-diesel/articleshow/87512919.cms",
-            "urlToImage": "https://static.toiimg.com/thumb/msid-87512913,width-1070,height-580,imgsize-106008,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
-            "publishedAt": "2021-11-03T16:10:00Z",
-            "content": "Centre cuts excise duty on petrol, diesel"
-        },
-        {
-            "source": {
-                "id": null,
-                "name": "Hindustan Times"
-            },
-            "author": "HT Tech",
-            "title": "Terror times in store for NASA's James Webb Space Telescope engineers - HT Tech",
-            "description": "NASA's team for the James Webb Space Telescope project has a date with “2 weeks of terror\".",
-            "url": "https://tech.hindustantimes.com/tech/news/terror-times-in-store-for-nasa-s-james-webb-space-telescope-engineers-71635954086139.html",
-            "urlToImage": "https://images.hindustantimes.com/tech/img/2021/11/03/1600x900/James_Webb_Space_Telescope_ESA_1635954655087_1635954696732.jpg",
-            "publishedAt": "2021-11-03T15:41:25Z",
-            "content": "The replacement for the National Aeronautics and Space Administration's (NASA) Hubble Space Telescope, the James Webb Space telescope is being readied for launch and it is almost ready to fly into sp… [+3219 chars]"
-        },
-        {
-            "source": {
-                "id": "google-news",
-                "name": "Google News"
-            },
-            "author": null,
-            "title": "Pak. denies airspace to Home minister inaugurated Srinagar-Sharjah flight - The Hindu",
-            "description": null,
-            "url": "https://news.google.com/__i/rss/rd/articles/CBMiiAFodHRwczovL3d3dy50aGVoaW5kdS5jb20vbmV3cy9uYXRpb25hbC9wYWtpc3Rhbi1yZWZ1c2VzLWdvLWZpcnN0cy1zcmluYWdhci1zaGFyamFoLWZsaWdodC10by1nby10aHJvdWdoLWl0cy1haXJzcGFjZS9hcnRpY2xlMzczMjE5MjEuZWNl0gGNAWh0dHBzOi8vd3d3LnRoZWhpbmR1LmNvbS9uZXdzL25hdGlvbmFsL3Bha2lzdGFuLXJlZnVzZXMtZ28tZmlyc3RzLXNyaW5hZ2FyLXNoYXJqYWgtZmxpZ2h0LXRvLWdvLXRocm91Z2gtaXRzLWFpcnNwYWNlL2FydGljbGUzNzMyMTkyMS5lY2UvYW1wLw?oc=5",
-            "urlToImage": null,
-            "publishedAt": "2021-11-03T15:40:44Z",
-            "content": null
-        },
-        {
-            "source": {
-                "id": "the-times-of-india",
-                "name": "The Times of India"
-            },
-            "author": "TIMESOFINDIA.COM",
-            "title": "BCCI appoints Rahul Dravid as head coach of Indian men's senior cricket team - Times of India",
-            "description": "Cricket News: Former captain Rahul Dravid was on Wednesday appointed as the head coach of the Indian men's cricket team by the BCCI. The former India captain will t",
-            "url": "https://timesofindia.indiatimes.com/sports/cricket/news/bcci-appoints-rahul-dravid-as-head-coach-of-indian-mens-senior-cricket-team/articleshow/87513437.cms",
-            "urlToImage": "https://static.toiimg.com/thumb/msid-87513417,width-1070,height-580,imgsize-19180,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
-            "publishedAt": "2021-11-03T15:27:00Z",
-            "content": "Congratulations Rahul Dravid for becoming the coach of Indian Mens Cricket Team. Indian players will be hugely ben https://t.co/gmY3kfvSRb\r\n— Venkatesh Prasad (@venkateshprasad) 1635953287000"
-        },
-        {
-            "source": {
-                "id": null,
-                "name": "Firstpost"
-            },
-            "author": null,
-            "title": "Highlights, New Zealand vs Scotland, T20 World Cup 2021 Cricket Match Updates: New Zealand win by 15 runs - Firstpost",
-            "description": "New Zealand vs Scotland Live Score, T20 World Cup 2021 Today's Match Updates: Check out Live Cricket Score, live streaming and updates, when and where to watch of NZ vs Scot today’s match live, playing 11 on Hotstar Cricket, Star Sports and DD Sports.",
-            "url": "https://www.firstpost.com/firstcricket/sports-news/highlights-new-zealand-vs-scotland-t20-world-cup-2021-cricket-match-updates-new-zealand-win-by-15-runs-10106751.html",
-            "urlToImage": "https://images.firstpost.com/wp-content/uploads/2021/11/Sodhi-dismissing-Kohli-640_AP.jpg",
-            "publishedAt": "2021-11-03T13:58:12Z",
-            "content": "Toggle between tabs to view scorecard and ball-by-ball commentary\r\nEngland vs Sri Lanka, Latest Updates, T20 World Cup 2021: New Zealand 172/5 (20 overs), Scotland 156/5 (20 overs)\r\nNew Zealand will … [+2410 chars]"
-        }
-    ]
+    static defaultProps = {
+        country: 'in',
+        pageSize: 6,
+        category: 'general'
+    }
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string,
+    }
+    
     constructor() {
         super();
         console.log("i am news  constructor");
         this.state = {
-            articles: this.articles,
+            articles: [],
             loading: false,
             page: 1
         }
