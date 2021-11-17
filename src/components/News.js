c2cf529feddb66498371088818a5a51d0b260822
Umesh Sahani <umeshsahani1998@gmail.com>
2021-11-17 16:05:11 +0530

    improve news.js ui for inifinite scroll

--- src/components/News.js
+++ src/components/News.js
@@ -73,39 +73,35 @@ export default class News extends Component {
     render() {
         return (
             <>
-                <div className="px-2 my-3">
-                    <div className="card">
-                        <h4 className="card-header text-center">E-Guptchar - Top {this.CapitalizeFirstLetter(this.props.category)} Headlines</h4>
-                        {/* {this.state.loading && <Spinner />} */}
-
-                        <div className="card-body">
-                            <InfiniteScroll
-                                dataLength={this.state.articles.length}
-                                next={this.fetchMoreData}
-                                hasMore={this.state.articles.length !== this.state.totalResults}
-                                loader={<Spinner />}
-                            >
-                                <div className="container">
 
 
-                                    <div className="row">
-                                        {this.state.articles.map((element) => {
-                                            return <div className="col-md-4 my-3" key={element.url}>
-                                                <NewsBox title={element.title ? element.title : "Title Not Available"} description={element.description ? element.description.slice(0, 150) : "Descipriton not available"} imageUrl={element.urlToImage ? element.urlToImage : "https://images.hindustantimes.com/tech/img/2021/11/03/1600x900/James_Webb_Space_Telescope_ESA_1635954655087_1635954696732.jpg"} newsUrl={element.url} sourceName={element.source.name} publishedAt={element.publishedAt} author={element.author} />
-                                            </div>
-                                        })}
-                                    </div>
+                <h4 className="text-center text-white bg-secondary p-2 mt-3">E-Guptchar - Top {this.CapitalizeFirstLetter(this.props.category)} Headlines</h4>
+                {/* {this.state.loading && <Spinner />} */}
+
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className="container-fluid px-5">
+                        <div className="row">
+                            {this.state.articles.map((element) => {
+                                return <div className="col-md-4 my-2" key={element.url}>
+                                    <NewsBox title={element.title ? element.title : "Title Not Available"} description={element.description ? element.description.slice(0, 150) : "Descipriton not available"} imageUrl={element.urlToImage ? element.urlToImage : "https://images.hindustantimes.com/tech/img/2021/11/03/1600x900/James_Webb_Space_Telescope_ESA_1635954655087_1635954696732.jpg"} newsUrl={element.url} sourceName={element.source.name} publishedAt={element.publishedAt} author={element.author} />
                                 </div>
-                            </InfiniteScroll>
-                            <hr />
-                            <div className="d-flex justify-content-between">
-                                <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr;&nbsp;Previous</button>
-
-                                <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next&nbsp;&rarr;</button>
-                            </div>
+                            })}
                         </div>
                     </div>
+                </InfiniteScroll>
+                <hr />
+                <div className="d-flex justify-content-between container">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr;&nbsp;Previous</button>
+
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next&nbsp;&rarr;</button>
                 </div>
+
+
             </>
         )
     }
