32912903dedae1181b3b42a10ffd234efe180b0d
Umesh Sahani <umeshsahani1998@gmail.com>
2021-11-17 15:56:25 +0530

    add infinte scroll features into News component

--- src/components/News.js
+++ src/components/News.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react'
 import NewsBox from './NewsBox'
 import Spinner from './Spinner'
 import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export default class News extends Component {
     static defaultProps = {
@@ -24,16 +26,21 @@ export default class News extends Component {
         this.state = {
             articles: [],
             loading: false,
-            page: 1
+            page: 1,
+            totalResults: 0
         }
-        document.title =`${this.CapitalizeFirstLetter(this.props.category)} - E-Guptchar || Umesh` ;
+        document.title = `${this.CapitalizeFirstLetter(this.props.category)} - E-Guptchar || Umesh`;
     }
     async UpdateNews() {
         const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=ec2115d017b3450b82eb5f9e083bd260&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parseData = await data.json(data);
-        this.setState({ articles: parseData.articles, totalResults: parseData.totalResults, loading: false })
+        this.setState({
+            articles: parseData.articles,
+            totalResults: parseData.totalResults,
+            loading: false
+        })
     }
     async componentDidMount() {
         console.log('cdm');
@@ -50,6 +57,18 @@ export default class News extends Component {
         this.setState({ page: this.state.page + 1 });
         this.UpdateNews();
     }
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 });
+        const url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=ec2115d017b3450b82eb5f9e083bd260&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let data = await fetch(url);
+        let parseData = await data.json(data);
+        this.setState({
+            articles: this.state.articles.concat(parseData.articles),
+            totalResults: parseData.totalResults,
+            loading: false
+        })
+    }
 
     render() {
         return (
@@ -57,15 +76,27 @@ export default class News extends Component {
                 <div className="px-2 my-3">
                     <div className="card">
                         <h4 className="card-header text-center">E-Guptchar - Top {this.CapitalizeFirstLetter(this.props.category)} Headlines</h4>
-                        {this.state.loading && <Spinner />}
+                        {/* {this.state.loading && <Spinner />} */}
+
                         <div className="card-body">
-                            <div className="row">
-                                {!this.state.loading && this.state.articles.map((element) => {
-                                    return <div className="col-md-4 my-3" key={element.url}>
-                                        <NewsBox title={element.title ? element.title : "Title Not Available"} description={element.description ? element.description.slice(0, 150) : "Descipriton not available"} imageUrl={element.urlToImage ? element.urlToImage : "https://images.hindustantimes.com/tech/img/2021/11/03/1600x900/James_Webb_Space_Telescope_ESA_1635954655087_1635954696732.jpg"} newsUrl={element.url} sourceName={element.source.name} publishedAt={element.publishedAt} author={element.author} />
+                            <InfiniteScroll
+                                dataLength={this.state.articles.length}
+                                next={this.fetchMoreData}
+                                hasMore={this.state.articles.length !== this.state.totalResults}
+                                loader={<Spinner />}
+                            >
+                                <div className="container">
+
+
+                                    <div className="row">
+                                        {this.state.articles.map((element) => {
+                                            return <div className="col-md-4 my-3" key={element.url}>
+                                                <NewsBox title={element.title ? element.title : "Title Not Available"} description={element.description ? element.description.slice(0, 150) : "Descipriton not available"} imageUrl={element.urlToImage ? element.urlToImage : "https://images.hindustantimes.com/tech/img/2021/11/03/1600x900/James_Webb_Space_Telescope_ESA_1635954655087_1635954696732.jpg"} newsUrl={element.url} sourceName={element.source.name} publishedAt={element.publishedAt} author={element.author} />
+                                            </div>
+                                        })}
                                     </div>
-                                })}
-                            </div>
+                                </div>
+                            </InfiniteScroll>
                             <hr />
                             <div className="d-flex justify-content-between">
                                 <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr;&nbsp;Previous</button>
