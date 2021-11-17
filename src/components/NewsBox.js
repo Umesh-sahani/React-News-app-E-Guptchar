63e0956604473463bfc8549a0748bce818168698
Umesh Sahani <umeshsahani1998@gmail.com>
2021-11-17 15:07:09 +0530

    add custom image as url icon

--- src/components/NewsBox.js
+++ src/components/NewsBox.js
@@ -7,13 +7,13 @@ export default class NewsBox extends Component {
         return (
             <>
                 <div className="card">
-                    <span class="badge badge-danger m-1 p-1" style={{ position: 'absolute' }}>{sourceName}</span>
+                    <span className="badge badge-danger m-1 p-1" style={{ position: 'absolute' }}>{sourceName}</span>
                     <img src={imageUrl} className="card-img-top" alt={title} />
 
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}...</p>
-                        <p class="card-text"><small class="text-black rounded bg-warning p-1">{author ? `By ${author}` : ''}  {new Date(`${publishedAt}`).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' })}</small></p>
+                        <p className="card-text"><small className="text-black rounded bg-warning p-1">{author ? `By ${author}` : ''}  {new Date(`${publishedAt}`).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' })}</small></p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
